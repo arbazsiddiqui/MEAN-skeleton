e9f9dcd748bf747304bdbeee935052068fbc18a7
Alexis Tyler <xo@wvvw.me>
2017-04-02 11:53:51 +0930

    convert var to const, cleanup excessive comments

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-// set up ======================================================================
-var express = require('express');
-var path = require('path');
-var app = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var passport = require('passport');
-var cookieParser = require('cookie-parser');
-var secret = require('./secret');
-var session = require('client-sessions');
-// configuration ===============================================================
-mongoose.connect(database.url);
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 8080;
+const database = require('./config/database');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const secret = require('./secret');
+const session = require('client-sessions');
+
+mongoose.connect(database.url, function(error) {
+    if (error) {
+        console.error(error);
+        process.exit();
+    }
+    console.info('Connected successfully to MongoDB');
+});
 require('./config/passport')(passport);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('sessionSecret', secret.sessionSecret);
 
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
 app.use(cookieParser(secret.sessionSecret));
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
 
 var auth = require('./app/controllers/authController')(passport);
 var todo = require('./app/controllers/todoController');
 
-
-// required for passport
-
 app.use(session({
   secret: secret.sessionSecret,
   cookieName: 'session'
-})); // session secret
+}));
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-
-
-// routes ======================================================================
+app.use(passport.session());
 app.use('/', auth);
 app.use('/todo', todo);
-// listen (start app with node server.js) ======================================
-app.listen(port);
-console.log("App listening on port " + port);
+
+app.listen(port, function() {
+    console.info('App listening on port', port);
+});
